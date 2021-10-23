710dbf03b511bf6ca612882dc380624316ce5c39
ahmedkamal6 <87461881+ahmedkamal6@users.noreply.github.com>
2021-10-23 17:01:14 +0200

    Update app.js

--- Project/app.js
+++ Project/app.js
@@ -1,32 +1,5 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
+ /* Start Helper Functions
  * 
 */
 function inView(el){
@@ -69,17 +42,3 @@ document.addEventListener('scroll',function(){
 }
 });
 
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
